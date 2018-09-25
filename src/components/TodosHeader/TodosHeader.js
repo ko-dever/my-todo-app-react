bf973b115f344205e4f053078bfc854844a43725
ko-dever <4114656+ko-dever@users.noreply.github.com>
2018-09-25 14:01:25 +0100

    Implement props typechecking for TodosHeader
    - Default props values added
    - Prop types added

--- src/components/TodosHeader/TodosHeader.js
+++ src/components/TodosHeader/TodosHeader.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 
@@ -8,10 +9,34 @@ const StyledHeader = styled.header`
 `;
 
 
-export default ( { nbTodos }) => (
+const TodosHeader = ( { nbTodos }) => (
   <StyledHeader>
 
-    <strong>My first TODO app</strong> (#{ nbTodos })
+    <strong>My first TODO app</strong>
+
+    { typeof nbTodos === 'undefined'
+      ? ''
+      : ` (#${ nbTodos })`
+    }
+
 
   </StyledHeader>
 );
+
+
+// Component's default props
+TodosHeader.defaultProps = {
+  nbTodos: 0,
+};
+
+
+// Component props typechecking
+TodosHeader.propTypes = {
+  nbTodos: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
+};
+
+
+export default TodosHeader;
