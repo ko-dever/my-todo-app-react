9a2a883caefc921b852a96a14fad34591cde8540
ko-dever <4114656+ko-dever@users.noreply.github.com>
2018-10-06 22:26:44 +0100

    Improve JSDoc and comments for TodosList

--- src/components/TodosList/TodosList.js
+++ src/components/TodosList/TodosList.js
@@ -10,6 +10,13 @@ const StyledList = styled.div`
 `;
 
 
+/**
+ * Component's function
+ *
+ * @param {Object} props - Component's props
+ * @param {Array} props.todos - List of todos
+ * @return {JSX.Element}
+ */
 const TodosList = ( { todos } ) => (
   <StyledList>
 
@@ -19,12 +26,18 @@ const TodosList = ( { todos } ) => (
 );
 
 
+/**
+ * Create the list of todos or a text if the list is empty
+ *
+ * @param {Array} todos - List of todos
+ * @return {JSX.Element} List of generated todos
+ */
 const generateTodos = function generateTodos( todos ) {
   if ( todos.length === 0 ) {
     return <em>Aucune tâche</em>;
   }
 
-  const markup = todos.map( (todo) => (
+  const markup = todos.map( todo => (
     <TodoItem
       key={ todo.id }
       todo={ todo }
